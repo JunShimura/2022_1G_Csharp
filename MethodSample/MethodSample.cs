4d6fca4179365f36cb2e6e3d953d9ee3d51af5ac
Jun Shimura <j.shimura@sugawara.ac.jp>
2022-06-02 17:33:07 +0900

    MethodSample

---
+++ MethodSample/MethodSample.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace MethodSample
+{
+    class MethodSampleClass
+    {
+        static void Main()
+        {
+            Console.WriteLine("変数を入力:");
+            int a =int.Parse(Console.ReadLine());
+            Console.WriteLine("変数を入力:");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("変数を入力:");
+            int c = int.Parse(Console.ReadLine());
+            var d = GetMax(a, b, c);
+            Console.WriteLine($"最大値は{d}");
+            return;
+        }
+        static int GetMax(int a, int b, int c)
+        {
+            var max = a; //最大値を格納する変数
+            if (max < b)
+            {
+                max = b;
+            }
+            if (max < c)
+            {
+                max = c;
+            }
+            return max;
+        }
+
+
+    }
+}
