1085275a75c75d71432aa7aec83be0d18c509598
Jun Shimura <j.shimura@sugawara.ac.jp>
2022-06-28 17:41:50 +0900

    Exception

---
+++ Exception/Exception.cs
@@ -0,0 +1,35 @@
+﻿namespace Exception
+{
+    internal class Exception
+    {
+        // 文字→整数に変換する関数CharToInt
+        static int CharToInt(char c)
+        {
+            if (c < '0' || c > '9')
+            {
+                throw new FormatException();
+            }
+            return c - '0';
+        }
+
+        // 文字列→整数に変換する関数StringToInt
+        static int StringToInt(string str)
+        {
+            int val = 0;
+            foreach (char c in str)
+            {
+                int i = CharToInt(c);
+                val = val * 10 + i;
+            }
+            return val;
+        }
+
+        static void Main()
+        {
+            Console.WriteLine($"{StringToInt("12345")}");
+
+            // 数字以外の文字が含まれている
+            Console.WriteLine($"{StringToInt("12a45")}");
+        }
+    }
+}
---
+++ TryCatch/TryCatch.cs
@@ -0,0 +1,51 @@
+﻿namespace TryCatch
+{
+    internal class TryCatch
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("数字を入力してください：");
+                string value = Console.ReadLine();
+                try
+                {
+                    var number = Int32.Parse(value);
+                    Console.WriteLine("{0} --> {1}", value, number);
+                }
+                catch (FormatException)
+                {   //変換できない文字
+                    Console.WriteLine("{0}: Bad Format", value);
+                    continue;
+                }
+                catch (OverflowException)
+                {//オーバーフロー
+                    Console.WriteLine("{0}: Overflow", value);
+                    continue;
+                }
+                catch
+                {
+                    Console.WriteLine("何かおかしい！");
+                    continue;
+                }
+                break;
+            }
+
+            int a;
+            while (true)
+            {
+                Console.Write("二つ目：数字を入力してください：");
+                bool sucess = int.TryParse(Console.ReadLine(), out a);
+                if (sucess)
+                {
+                    Console.WriteLine($"変換できました、値は{a}");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"変換できませんでした");
+                }
+            }
+        }
+    }
+}
