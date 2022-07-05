07f8a4f231923a5bb4d5db72da7f1f0a4f3db164
Jun Shimura <j.shimura@sugawara.ac.jp>
2022-07-05 17:30:16 +0900

    GCM

--- RefOut/Program.cs
+++ RefOut/Program.cs
@@ -6,8 +6,11 @@ namespace RefOut
     {
         static void Main()
         {
-            int a = 100;
-            int a;
+            int x = 16384;
+            int y = 48;
+            int g = GetGcm(x, y);
+            Console.WriteLine($"gcm({x},{y})={g}");
+/*            int a = 100;
             //Console.Write($"a={a} → ");
             Test(ref a);
             Console.WriteLine($"{a}");
@@ -15,6 +18,7 @@ namespace RefOut
             //Console.Write($"b={b} → ");   //初期化前で存在しないのでエラー
             TestOut(out b);
             Console.WriteLine($"{b}");
+*/        
         }
         static void Test(ref int a)
         {
@@ -23,6 +27,23 @@ namespace RefOut
         static void TestOut(out int a)
         {
             a = 10; // メソッド内で値を書き換える
+        }
+        static int GetGcm(int a,int b)
+        {
+            if (a < b)
+            {
+                var t = a;
+                a = b;
+                b = t;
+            }
+            int r = b;// ユークリッドの互除法
+            while (r != 0)
+            {
+                b = r;
+                r = a % b;
+                a= b;
+            }
+            return b;
         }
     }
 }
