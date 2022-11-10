966539cd84a423ac49ea8e8094218ac2ce34d536
Jun Shimura <junshimura@gmail.com>
2022-11-10 17:31:55 +0900

    Virtual

---
+++ List7_6/NonVirtualTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Base
+{
+    // 基底クラスBaseのTest()メソッド
+    public void Test()
+    {
+        Console.WriteLine("Base.Test()");
+    }
+}
+
+class Derived : Base
+{
+    // 派生クラスDerivedのTest()メソッド
+    public new void Test()
+    {
+        Console.WriteLine("Derived.Test()");
+    }
+}
+
+class NonVirtualTest
+{
+    static void Main()
+    {
+        Base a = new Base();
+        // 変数aの静的な型はBaseなので
+        // 基底クラスのTest()メソッドが呼ばれる
+        a.Test();
+
+        Base b = new Derived();
+        // 変数bの静的な型はBaseなので
+        // 基底クラスのTest()メソッドが呼ばれる
+        b.Test();
+
+        Derived c = new Derived();
+        // 変数cの静的な型はDerivedなので
+        // 派生クラスのTest()メソッドが呼ばれる
+        c.Test();
+        Base d = c;
+        d.Test();   //　Base.Testが呼ばれる
+    }
+}
---
+++ List7_7/VirtualTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Base
+{
+    // 基底クラスBaseのTest()メソッド（virtual修飾子付き）
+    public virtual void Test()
+    {
+        Console.WriteLine("Base.Test()");
+    }
+}
+
+class Derived : Base
+{
+    // 派生クラスDerivedのTest()メソッド（override修飾子は後述）
+    public override void Test()
+    {
+        Console.WriteLine("Derived.Test()");
+    }
+}
+
+class VirtualTest
+{
+    static void Main()
+    {
+        Base a = new Base();
+        // 変数aの動的なクラスがBaseなので
+        // 基底クラスのTest()メソッドが呼ばれる
+        a.Test();
+
+        Base b;
+         Derived b2 = new Derived();
+        b = b2;
+        // 変数bの動的なクラスがDerivedなので
+        // 派生クラスのTest()メソッドが呼ばれる
+        b.Test();
+
+        Derived c = new Derived();
+        // 変数cの動的なクラスがDerivedなので
+        // 派生クラスのTest()メソッドが呼ばれる
+        c.Test();
+        Base d = c;
+        d.Test();   // Derived.Testが呼ばれる
+    }
+}
