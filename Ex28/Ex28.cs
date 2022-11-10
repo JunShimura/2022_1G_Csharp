183c3af35c2c689b1cba3c9bb7685d10544210f9
Jun Shimura <junshimura@gmail.com>
2022-11-10 12:51:30 +0900

    Class

--- Ex28/Ex28.cs
+++ Ex28/Ex28.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // 底面の直角三角形の底辺3,底面の直角三角形のの高さ4,高さ5の三角柱
+            TriangularPrism triangularPrism1 = new TriangularPrism(3, 4, 5);
+            Console.WriteLine($"表面積={triangularPrism1.GetSurface()}\n 体積 ={triangularPrism1.GetVolume()}");
+            // 底面の三角形の３辺の長さが3,4,5、高さが6
+            TriangularPrism triangularPrism2 = new TriangularPrism(3, 4, 5,6);
+            Console.WriteLine($"表面積={triangularPrism2.GetSurface()}\n 体積 ={triangularPrism2.GetVolume()}");
         }
     }
 }
