92856e93f1dc280725f086edb8b8b577e4941c13
Jun Shimura <junshimura@gmail.com>
2022-12-22 13:07:14 +0900

    Ex33

---
+++ Ex33/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace EX33_hint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RectAngle rectAngle1 = new RectAngle(3, 5);
+            RectAngle rectAngle2 = new RectAngle(5, 3);
+            if (rectAngle1 == rectAngle2)
+            {
+                Console.WriteLine("rectAngle1とrectAngle2は等しい");
+            }
+            else
+            {
+                Console.WriteLine("rectAngle1とrectAngle2は等しくない");
+            }
+        }
+    }
+}
