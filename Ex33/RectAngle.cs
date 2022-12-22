2b508f1f68963990160a6ad61be9c7719c395a90
Jun Shimura <junshimura@gmail.com>
2022-12-22 17:33:11 +0900

    Ex33

--- Ex33/RectAngle.cs
+++ Ex33/RectAngle.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Text;
 
@@ -26,6 +27,7 @@ namespace EX33_hint
             width = this.width;
             height = this.height;
         }
+
         public static bool operator ==(RectAngle rectAngle1, RectAngle rectAngle2)
         {
             return rectAngle1.width == rectAngle2.width
@@ -33,10 +35,34 @@ namespace EX33_hint
                 || rectAngle1.width == rectAngle2.height
                 && rectAngle1.height == rectAngle2.width;
         }
-        
         public static bool operator !=(RectAngle rectAngle1, RectAngle rectAngle2)
         {
             return !(rectAngle1 == rectAngle2);
+        }
+        public static RectAngle operator +(RectAngle rectAngle1, RectAngle rectAngle2)
+        {
+            RectAngle ans = new RectAngle(float.MaxValue,float.MaxValue);
+            List<RectAngle> candidates = new List<RectAngle>()
+            {
+                Plus(rectAngle1.width,rectAngle2.width, rectAngle1.height,rectAngle2.height),
+                Plus(rectAngle1.width, rectAngle2.height, rectAngle1.height, rectAngle2.width),
+                Plus(rectAngle1.height, rectAngle2.width, rectAngle1.width, rectAngle2.height),
+                Plus(rectAngle1.height, rectAngle2.height, rectAngle1.width, rectAngle2.width)
+            };
+            //ans = candidates.Min(candidate => candidate.GetSurface);
+            foreach (RectAngle candidate in candidates)
+            {
+                if (ans.GetSurface() > candidate.GetSurface())
+                {
+                    ans = candidate;
+                }
+            }
+
+            return ans;
+        }
+        private static RectAngle Plus(float l1, float l2, float l3, float l4)
+        {
+            return new RectAngle(l1 + l2, (l3 > l4) ? l3 : l4);
         }
     }
 }
